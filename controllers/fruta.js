593b05e26013c7646aff9f6ae69ba49f45e4ae21
Arano Orkatz <oarano@lekooa.com>
2018-03-22 08:12:08 +0100

    add controller && routes saveFruta

--- controllers/fruta.js
+++ controllers/fruta.js
@@ -1,11 +1,40 @@
 'use strict'
 
+var Fruta = require('../models/fruta.js');
+
 function pruebas(req, res) {
     res.status(200).send({
         message: 'Esta ruta es de prueba en mi apiRestful'
     });
 }
 
+function saveFruta(req, res) {
+    var fruta = new Fruta();
+
+    var params = req.body;
+
+    if (params.nombre) {
+        fruta.nombre = params.nombre;
+        fruta.color = params.color;
+        fruta.temporada = params.temporada;
+
+        fruta.save((err, frutaStored) => {
+            if (err) {
+                res.status(500).send({ message: 'error en el servidor' })
+            } else {
+                if (frutaStored) {
+                    res.status(200).send({ fruta: frutaStored });
+                } else {
+                    res.status(200).send({ message: 'La fruta no se ha guardado' });
+                }
+            }
+        });
+    } else {
+        res.status(200).send({ message: 'El nombre de la fruta es obligatorio' });
+    }
+}
+
 module.exports = {
-    pruebas
+    pruebas,
+    saveFruta
 };
--- routes/fruta.js
+++ routes/fruta.js
@@ -6,5 +6,6 @@ var frutaController = require('../controllers/fruta');
 var api = express.Router();
 
 api.get('/pruebas', frutaController.pruebas);
+api.post('/fruta', frutaController, saveFruta)
 
 module.exports = api; 
